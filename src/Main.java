4ae71204f0a11e7de350ae1d623cdb048378bebe
zhengzhangyang <1933964250@qq.com>
2019-12-10 17:27:05 +0800

    First commit all.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+/**
+ * @author 郑章洋
+ * @date 2019-11-19 10:08
+ */
+public class Main {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.out.println(12345+5432l);
+    }
+    public static boolean isOdd(int i){
+        return (i & 1) != 0;
+    }
+
+
+}
